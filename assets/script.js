2039a522f9c2011a53b6201b16bbaf1cfeb6ab65
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2023-11-01 20:05:27 +0100

    creating a const variable at the script and adding questions

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+const questions = [
+{
+    question: "What is the most essential factor for the photosynthesis in plants?", 
+    choices: ["A. Carbon Dioxide", "B. Water" , "C. Sunlight", "D. Fertilizer"],
+    correct: "C"
+},    
+
+{
+    question: "How often should you water succulent plants?", 
+    choices: ["A. Daily", "B. Weekly" , "C. Monthly", "D. Irregularly"],
+    correct: "D"
+},    
+
+{
+    question: "HWhich of the following pests is common among houseplants and can damage the leaves?", 
+    choices: ["A. Aphids", "B. Bees" , "C. Ladybugs", "D. Earthworms"],
+    correct: "A"
+},    
+
+
+];

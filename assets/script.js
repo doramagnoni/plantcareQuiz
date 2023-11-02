52ee94afe191ed439d40f6c3706448c38626b739
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2023-11-02 21:56:34 +0100

    changing the variables and the functions in the js in order to manipulate the index. Adding corrections to the index in order to make it responsive to the js. Styling further he body of the text.

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,66 @@
+const questions = [{
+        question: "What is the most essential factor for the photosynthesis in plants?",
+        answer: ["Carbon Dioxide", "Water", "Sunlight", "Fertilizer"],
+        correct: "Sunlight"
+    },
 
-const questions = [
-{
-    question: "What is the most essential factor for the photosynthesis in plants?", 
-    choices: ["A. Carbon Dioxide", "B. Water" , "C. Sunlight", "D. Fertilizer"],
-    correct: "C"
-},    
+    {
+        question: "How often should you water succulent plants?",
+        answer: ["Daily", "Weekly", "Monthly", "Irregularly"],
+        correct: "Monthly"
+    },
 
-{
-    question: "How often should you water succulent plants?", 
-    choices: ["A. Daily", "B. Weekly" , "C. Monthly", "D. Irregularly"],
-    correct: "D"
-},    
-
-{
-    question: "Which of the following pests is common among houseplants and can damage the leaves?", 
-    choices: ["A. Aphids", "B. Bees" , "C. Ladybugs", "D. Earthworms"],
-    correct: "A"
-},    
-//more quiestion here
+    {
+        question: "Which of the following pests is common among houseplants and can damage the leaves?",
+        answer: ["Aphids", "Bees", "Ladybugs", "Earthworms"],
+        correct: "Aphids"
+    },
+    // Add more quiestion here
 
 ];
 
-function loadQuestion() {
+let currentQuestion = 0;
+let score = 0;
 
+const questionElement = document.getElementById("question");
+const answerButtons = document.querySelectorAll(".answer-btn");
+const scoreElement = document.getElementById("score");
+const feedbackElement = document.getElementById("feedback");
+
+function startQuiz() {
+  currentQuestion = 0;
+  score = 0;
+  showQuestion();
 }
 
-function checkAnswer() {
-
+function showQuestion() {
+  if (currentQuestion < questions.length) {
+      questionElement.innerText = questions[currentQuestion].question;
+      for (let i = 0; i < answerButtons.length; i++) {
+          answerButtons[i].innerText = questions[currentQuestion].answers[i];
+          answerButtons[i].addEventListener("click", checkAnswer);
+      }
+  } else {
+      endQuiz();
+  }
 }
 
-function showResult() {
-
+function checkAnswer(event) {
+  const selectedAnswer = event.target.innerText;
+  if (selectedAnswer === questions[currentQuestion].correct) {
+      score++;
+      feedbackElement.innerText = "Correct!";
+  } else {
+      feedbackElement.innerText = "Incorrect.";
+  }
+  scoreElement.innerText = score;
+  currentQuestion++;
+  showQuestion();
 }
+
+function endQuiz() {
+  questionElement.innerText = "Quiz Complete!";
+  answerButtons.forEach(button => button.style.display = "none");
+}
+
+startQuiz();

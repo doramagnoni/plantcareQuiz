ff001cb2aab8c2f7fe54624c09d3f3b92cc897df
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2023-11-03 12:41:23 +0100

    adding br in between the paragraphs in the index, styling the quiz container in the middle of the page, adding numbers to the quiestion in the js file

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,53 @@
 const questions = [{
-    question: "What is the most essential factor for the photosynthesis in plants?",
+    question: "1. What is the most essential factor for the photosynthesis in plants?",
     answers: ["Carbon Dioxide", "Water", "Sunlight", "Fertilizer"],
     correct: "Sunlight"
   },
 
   {
-    question: "How often should you water succulent plants?",
+    question: "2. How often should you water succulent plants?",
     answers: ["Daily", "Weekly", "Monthly", "Irregularly"],
     correct: "Monthly"
   },
 
   {
-    question: "Which of the following pests is common among houseplants and can damage the leaves?",
+    question: "3. Which of the following pests is common among houseplants and can damage the leaves?",
     answers: ["Aphids", "Bees", "Ladybugs", "Earthworms"],
     correct: "Aphids"
   },
 
   {
-    question: "Which of the following is NOT a suitable plant potting mix ingredient?",
+    question: "4. Which of the following is NOT a suitable plant potting mix ingredient?",
     answers: ["Perlite", "Sand", "Garden Soil", "Cofee Grounds"],
     correct: "Cofee Grounds"
   },
 
   {
-    question: "What is the purpose of pruning in plant care?",
+    question: "5. What is the purpose of pruning in plant care?",
     answers: ["To make the plant grow faster", "To remove dead or diseased parts", "To make the plant's leaves bigger", "To attract more butterflies"],
     correct: "To remove dead or diseased parts"
   },
 
   {
-    question: "Which of the following is a low-light houseplant that's suitable for spaces with minimal sunlight?",
+    question: "6. Which of the following is a low-light houseplant that's suitable for spaces with minimal sunlight?",
     answers: ["Snake Plant", "Aloe Vera", "Orchid", "Fiddle Leaf Fig"],
     correct: "Orchid"
   },
 
   {
-    question: "Which of the following is a common method for propagating houseplants?",
+    question: "7. Which of the following is a common method for propagating houseplants?",
     answers: ["Division", "Singing to the Plants", "Giving them chocolate", "Playing clasical music"],
     correct: "Division"
   },
 
   {
-    question: "When repotting a plant, what size should the new pot be compared to the old pot?",
+    question: "8. When repotting a plant, what size should the new pot be compared to the old pot?",
     answers: ["Smaller", "The same size", "Slightly larger", "Much larger"],
     correct: "Slightly larger"
   },
 
   {
-    question: "What is the purpose of a humidity tray for indoor plants?",
+    question: "9. What is the purpose of a humidity tray for indoor plants?",
     answers: ["To serve as a coaster for the plant", "To catch excess water when watering", "To increase humidity around the plant", "To deter pests"],
     correct: "To increase humidity around the plant"
   },

9c30fd2a254736dc8f686a182830b92438901efb
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2023-11-03 10:51:44 +0100

    playing around with the background color of the answers buttons

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,59 @@
 const questions = [{
-        question: "What is the most essential factor for the photosynthesis in plants?",
-        answers: ["Carbon Dioxide", "Water", "Sunlight", "Fertilizer"],
-        correct: "Sunlight"
-    },
+    question: "What is the most essential factor for the photosynthesis in plants?",
+    answers: ["Carbon Dioxide", "Water", "Sunlight", "Fertilizer"],
+    correct: "Sunlight"
+  },
 
-    {
-        question: "How often should you water succulent plants?",
-        answers: ["Daily", "Weekly", "Monthly", "Irregularly"],
-        correct: "Monthly"
-    },
+  {
+    question: "How often should you water succulent plants?",
+    answers: ["Daily", "Weekly", "Monthly", "Irregularly"],
+    correct: "Monthly"
+  },
 
-    {
-        question: "Which of the following pests is common among houseplants and can damage the leaves?",
-        answers: ["Aphids", "Bees", "Ladybugs", "Earthworms"],
-        correct: "Aphids"
-    },
+  {
+    question: "Which of the following pests is common among houseplants and can damage the leaves?",
+    answers: ["Aphids", "Bees", "Ladybugs", "Earthworms"],
+    correct: "Aphids"
+  },
 
-    {
-      question: "Which of the following is NOT a suitable plant potting mix ingredient?",
-      answers: ["Perlite", "Sand", "Garden Soil", "Cofee Grounds"],
-      correct: "Cofee Grounds"
+  {
+    question: "Which of the following is NOT a suitable plant potting mix ingredient?",
+    answers: ["Perlite", "Sand", "Garden Soil", "Cofee Grounds"],
+    correct: "Cofee Grounds"
   },
 
   {
     question: "What is the purpose of pruning in plant care?",
     answers: ["To make the plant grow faster", "To remove dead or diseased parts", "To make the plant's leaves bigger", "To attract more butterflies"],
     correct: "To remove dead or diseased parts"
-},
+  },
 
-{
-  question: "Which of the following is a low-light houseplant that's suitable for spaces with minimal sunlight?",
-  answers: ["Snake Plant", "Aloe Vera", "Orchid", "Fiddle Leaf Fig"],
-  correct: "Orchid"
-},
+  {
+    question: "Which of the following is a low-light houseplant that's suitable for spaces with minimal sunlight?",
+    answers: ["Snake Plant", "Aloe Vera", "Orchid", "Fiddle Leaf Fig"],
+    correct: "Orchid"
+  },
 
-{
-  question: "Which of the following is a common method for propagating houseplants?",
-  answers: ["Division", "Singing to the Plants", "Giving them chocolate", "Playing clasical music"],
-  correct: "Division"
-},
+  {
+    question: "Which of the following is a common method for propagating houseplants?",
+    answers: ["Division", "Singing to the Plants", "Giving them chocolate", "Playing clasical music"],
+    correct: "Division"
+  },
 
-{
-  question: "When repotting a plant, what size should the new pot be compared to the old pot?",
-  answers: ["Smaller", "The same size", "Slightly larger", "Much larger"],
-  correct: "Slightly larger"
-},
+  {
+    question: "When repotting a plant, what size should the new pot be compared to the old pot?",
+    answers: ["Smaller", "The same size", "Slightly larger", "Much larger"],
+    correct: "Slightly larger"
+  },
 
-{
-  question: "What is the purpose of a humidity tray for indoor plants?",
-  answers: ["To serve as a coaster for the plant", "To catch excess water when watering", "To increase humidity around the plant", "To deter pests"],
-  correct: "To increase humidity around the plant"
-},
+  {
+    question: "What is the purpose of a humidity tray for indoor plants?",
+    answers: ["To serve as a coaster for the plant", "To catch excess water when watering", "To increase humidity around the plant", "To deter pests"],
+    correct: "To increase humidity around the plant"
+  },
 
-    
-    // Add more quiestion here
+
+  // Add more quiestion here
 
 ];
 
@@ -73,23 +73,23 @@ function startQuiz() {
 
 function showQuestion() {
   if (currentQuestion < questions.length) {
-      questionElement.innerText = questions[currentQuestion].question;
-      for (let i = 0; i < answerButtons.length; i++) {
-          answerButtons[i].innerText = questions[currentQuestion].answers[i];
-          answerButtons[i].addEventListener("click", checkAnswer);
-      }
+    questionElement.innerText = questions[currentQuestion].question;
+    for (let i = 0; i < answerButtons.length; i++) {
+      answerButtons[i].innerText = questions[currentQuestion].answers[i];
+      answerButtons[i].addEventListener("click", checkAnswer);
+    }
   } else {
-      endQuiz();
+    endQuiz();
   }
 }
 
 function checkAnswer(event) {
   const selectedAnswer = event.target.innerText;
   if (selectedAnswer === questions[currentQuestion].correct) {
-      score++;
-      feedbackElement.innerText = "Correct!";
+    score++;
+    feedbackElement.innerText = "Correct!";
   } else {
-      feedbackElement.innerText = "Incorrect.";
+    feedbackElement.innerText = "Incorrect.";
   }
   scoreElement.innerText = score;
   currentQuestion++;
@@ -97,7 +97,7 @@ function checkAnswer(event) {
 }
 
 function endQuiz() {
-  if (score===questions.length) {
+  if (score === questions.length) {
     feedbackElement.innerText = "Congratulations! You are a plant expert and your green friends are lucky to have you!";
   } else if (score >= questions.length / 2) {
     feedbackElement.innerText = "Great job! You have a good understanfing of plant care!";
